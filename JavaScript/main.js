c19a25e01da39a760cad60d8f0071ed82d6ee1c6
Leandro Ezequiel Raymondi Rossa <ezequielraymondi@gmail.com>
2022-08-03 22:59:49 -0300

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,7 @@
 // ADICIONAR NUEVO PRODUCTO
 
 class Productos {
-    constructor(nombre, categoria, precio, stock){
+    constructor(nombre, categoria, precio, stock) {
         this.nombre = nombre;
         this.categoria = categoria;
         this.precio = (precio);
@@ -12,8 +12,8 @@ class Productos {
         if (this.stock >= cantidad) {
             this.stock = this.stock - cantidad;
             console.log(`quedan ${this.stock}`)
-        }else{
-            alert(`No tenemos esa cantidad de stock, quedan ${this.stock}`)
+        } else {
+            alert(`No tenemos esa cantidad de stock, quedan ${this.stock}`);
         }
         return this.stock;
     }
@@ -25,12 +25,12 @@ class Productos {
 }
 
 const listaProductos = [
-    {nombre: "topcoat", categoria: "esmaltes", precio: 1200, stock: 10},
-    {nombre: "esmalte", categoria: "esmaltes", precio: 890, stock: 35},
-    {nombre: "torno", categoria: "manicura", precio: 11999, stock: 10},
-    {nombre: "cabina", categoria: "manicura", precio: 4500, stock: 10},
-    {nombre: "lima", categoria: "manicura", precio: 100, stock: 50},
-    {nombre: "repujador", categoria: "manicura", precio: 150, stock: 15}
+    { nombre: "topcoat", categoria: "esmaltes", precio: 1200, stock: 10 },
+    { nombre: "esmalte", categoria: "esmaltes", precio: 890, stock: 35 },
+    { nombre: "torno", categoria: "manicura", precio: 11999, stock: 10 },
+    { nombre: "cabina", categoria: "manicura", precio: 4500, stock: 10 },
+    { nombre: "lima", categoria: "manicura", precio: 100, stock: 50 },
+    { nombre: "repujador", categoria: "manicura", precio: 150, stock: 15 }
 ]
 
 const agregarProducto = () => {
@@ -38,23 +38,25 @@ const agregarProducto = () => {
     let categoria = prompt("¿De qué categoría es el producto?");
     let precio = parseFloat(prompt("¿Cuál es el precio que quieres asignarle a tu producto?"));
     let stock = parseInt(prompt("¿Cuántos productos del mismo quieres vender?"));
-    if ((typeof precio === 'number') && ( typeof stock === 'number')){
-            let nuevoProducto = new Productos(nombre, categoria, precio, stock);
-            listaProductos.push(nuevoProducto);
-            console.log(listaProductos);
+    if ((typeof precio === 'number') && (typeof stock === 'number')) {
+        let nuevoProducto = new Productos(nombre, categoria, precio, stock);
+        listaProductos.push(nuevoProducto);
+        console.log(listaProductos);
     } else {
-        alert(`Los datos ingresados no son números. Vuelve a ingresarlos.`)
+        alert(`Los datos ingresados no son números. Vuelve a ingresarlos.`);
     }
 }
 
 // LISTA DE STOCK + IVA
-const stockIva = listaProductos.map((producto) => {return {
+const stockIva = listaProductos.map((producto) => {
+    return {
         nombre: producto.nombre,
         categoría: producto.categoria,
         precio: producto.precio * 1.21,
-        stock: producto.stock 
-    }}
-    )
+        stock: producto.stock
+    }
+}
+)
 
 // FILTRO PRECIOS
 let menorMayor = document.querySelector(".filterPrecio");
@@ -64,7 +66,7 @@ const filterPrecio = () => {
     let search = listaProductos.filter(producto => producto.precio <= filterUsuario);
     if (search != `undefined`) {
         console.log(search);
-    }else console.log(`No se ha encontrado productos en ese rango de precio.`)
+    } else console.log(`No se ha encontrado productos en ese rango de precio.`);
 }
 
 menorMayor.addEventListener("click", filterPrecio);
@@ -74,10 +76,128 @@ let cambiarTitulo = document.querySelector(".cambiarTitulo");
 
 cambiarTitulo.addEventListener("click", () => {
     let titulo = document.querySelector(".titulo");
-    titulo.innerHTML = (prompt("¿Cuál es el título que quieres colocar?"))
+    titulo.innerHTML = (prompt("¿Cuál es el título que quieres colocar?"));
     return titulo.innerText
 })
 
-// ELIMINAR PRODUCTOS
+//Carrito 1.0
 
-// Buscar la manera de hacer esta funcion, filter, find y recortar el index, etc.
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
+
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+
+const shoppingCartItemsContainer = document.querySelector(
+    '.shoppingCartItemsContainer'
+);
+
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
+
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImage = item.querySelector('.item-image').src;
+
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+}
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+        'shoppingCartItemTitle'
+    );
+    for (let i = 0; i < elementsTitle.length; i++) {
+        if (elementsTitle[i].innerText === itemTitle) {
+            let elementQuantity = elementsTitle[
+                i
+            ].parentElement.parentElement.parentElement.querySelector(
+                '.shoppingCartItemQuantity'
+            );
+            elementQuantity.value++;
+            $('.toast').toast('show');
+            updateShoppingCartTotal();
+            return;
+        }
+    }
+
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+  <div class="row shoppingCartItem">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImage} class="shopping-cart-image">
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                    value="1">
+                <button class="btn btn-danger buttonDelete" type="button">X</button>
+            </div>
+        </div>
+    </div>`;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    shoppingCartRow
+        .querySelector('.buttonDelete')
+        .addEventListener('click', removeShoppingCartItem);
+
+    shoppingCartRow
+        .querySelector('.shoppingCartItemQuantity')
+        .addEventListener('change', quantityChanged);
+
+    updateShoppingCartTotal();
+}
+
+function updateShoppingCartTotal() {
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+
+    shoppingCartItems.forEach((shoppingCartItem) => {
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+            '.shoppingCartItemPrice'
+        );
+        const shoppingCartItemPrice = Number(
+            shoppingCartItemPriceElement.textContent.replace('€', '')
+        );
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+            '.shoppingCartItemQuantity'
+        );
+        const shoppingCartItemQuantity = Number(
+            shoppingCartItemQuantityElement.value
+        );
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+    });
+    shoppingCartTotal.innerHTML = `${total.toFixed(2)}€`;
+}
+
+function removeShoppingCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
+}
+
+function quantityChanged(event) {
+    const input = event.target;
+    input.value <= 0 ? (input.value = 1) : null;
+    updateShoppingCartTotal();
+}
+
+function comprarButtonClicked() {
+    shoppingCartItemsContainer.innerHTML = '';
+    updateShoppingCartTotal();
+}
+// API.JSON es algo que quiero implementar luego de ver bien el tema fetch, async, etc, por eso está dentro del root
