8bab1bfb8749d3cadb55b9d3583750a5ecf59c91
Leandro Ezequiel Raymondi Rossa <ezequielraymondi@gmail.com>
2022-08-23 11:05:59 -0300

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,13 @@
 // ADICIONAR NUEVO PRODUCTO
 
 class Productos {
-    constructor(nombre, categoria, precio, stock) {
-        this.nombre = nombre;
-        this.categoria = categoria;
+    constructor(id, nombre, categoria, precio, stock, thumbnaiUrl) {
+        this.id = (id);
+        this.nombre = (nombre);
+        this.categoria = (categoria);
         this.precio = (precio);
         this.stock = (stock);
+        this.thumbnaiUrl = (thumbnaiUrl);
     }
 
     comprar(cantidad) {
@@ -24,15 +26,6 @@ class Productos {
     }
 }
 
-const listaProductos = [
-    { nombre: "topcoat", categoria: "esmaltes", precio: 1200, stock: 10 },
-    { nombre: "esmalte meline", categoria: "esmaltes", precio: 890, stock: 35 },
-    { nombre: "torno", categoria: "manicura", precio: 11999, stock: 10 },
-    { nombre: "cabina", categoria: "manicura", precio: 4500, stock: 10 },
-    { nombre: "lima", categoria: "manicura", precio: 100, stock: 50 },
-    { nombre: "repujador", categoria: "manicura", precio: 150, stock: 15 }
-]
-
 const agregarProducto = () => {
     let nombre = prompt("¿Cuál es el nombre del producto?");
     let categoria = prompt("¿De qué categoría es el producto?");
@@ -46,39 +39,6 @@ const agregarProducto = () => {
         alert(`Los datos ingresados no son números. Vuelve a ingresarlos.`);
     }
 }
-
-// LISTA DE STOCK + IVA
-const stockIva = listaProductos.map((producto) => {
-    return {
-        nombre: producto.nombre,
-        categoría: producto.categoria,
-        precio: producto.precio * 1.21,
-        stock: producto.stock
-    }
-}
-)
-
-// FILTRO PRECIOS
-let menorMayor = document.querySelector(".filterPrecio");
-
-const filterPrecio = () => {
-    let filterUsuario = parseFloat(prompt("¿Hasta que precio quieres buscar?"));
-    let search = listaProductos.filter(producto => producto.precio <= filterUsuario);
-    if (search != `undefined`) {
-        console.log(search);
-    } else console.log(`No se ha encontrado productos en ese rango de precio.`);
-}
-
-menorMayor.addEventListener("click", filterPrecio);
-
-//CAMBIAR TITULO
-let cambiarTitulo = document.querySelector(".cambiarTitulo");
-
-cambiarTitulo.addEventListener("click", () => {
-    let titulo = document.querySelector(".titulo");
-    titulo.innerHTML = (prompt("¿Cuál es el título que quieres colocar?"));
-    return titulo.innerText
-})
 
 //CARRITO 1.0
 
@@ -239,8 +199,20 @@ const btnAccion = e => {
             text: `Se eliminó un ${producto.title} del carrito`,
             duration: 1500,
             style: {background:"#e8e8e8", color:"black"},
-            onClick: () => {close: true}
         }).showToast();
     
     e.stopPropagation();
 }}
+
+// FILTRO PRECIOS
+let menorMayor = document.querySelector(".filterPrecio");
+
+const filterPrecio = () => {
+    let filterUsuario = parseFloat(prompt("¿Hasta que precio quieres buscar?"));
+    let search = fetchData.filter(producto => producto.precio <= filterUsuario);
+    if (search != `undefined`) {
+        pintarCard();
+    } else console.log(`No se ha encontrado productos en ese rango de precio.`);
+}
+
+menorMayor.addEventListener("click", filterPrecio);
