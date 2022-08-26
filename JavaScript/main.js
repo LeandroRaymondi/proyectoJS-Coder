101d136ca5c6b99ae0779c397794b4fe38aa0e2e
Leandro Ezequiel Raymondi Rossa <ezequielraymondi@gmail.com>
2022-08-26 18:38:35 -0300

    cannot add a new product

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,5 +1,9 @@
 // ADICIONAR NUEVO PRODUCTO
 
+// Crear la función agregarProducto que mediante los promps obtenga el nombre, categoria, precio, stock 
+// y que el ID se haga haciendo una lectura de api.JSON y sumandole ++ (uno) al máximo ID registrado,
+// la función de agregarProducto además tiene que volver a pintar las cartas de productos en el index, obviamente agregando el nuevo producto ingresado
+// esta función además va a tener otra función adentro que va a escribir el nuevo objeto creado dentro de api.JSON
 class Productos {
     constructor(id, nombre, categoria, precio, stock, thumbnaiUrl) {
         this.id = (id);
@@ -19,26 +23,33 @@ class Productos {
         }
         return this.stock;
     }
-
-    cargarStock(cantidad) {
-        this.stock = this.stock + cantidad;
-        //Agregar un return de true or false, para tener respuesta de la función y ver si se cargó bien el stock
-    }
 }
 
-const agregarProducto = () => {
+const addProduct = document.querySelector(".addProduct");
+
+const agregarProducto = (data) => {
+    let id = 7 ;
     let nombre = prompt("¿Cuál es el nombre del producto?");
     let categoria = prompt("¿De qué categoría es el producto?");
     let precio = parseFloat(prompt("¿Cuál es el precio que quieres asignarle a tu producto?"));
     let stock = parseInt(prompt("¿Cuántos productos del mismo quieres vender?"));
+
     if ((typeof precio === 'number') && (typeof stock === 'number')) {
-        let nuevoProducto = new Productos(nombre, categoria, precio, stock);
-        listaProductos.push(nuevoProducto);
-        console.log(listaProductos);
+        let nuevoProducto = new Productos(id, nombre, categoria, precio, stock, thumbnaiUrl);
+        fetch ("api.json", {
+            method: "POST",
+            headers: {'content-type':'application/json; charset=UTF-8'},
+            body: JSON.stringify(nuevoProducto)
+        })
+        .then(JSON.stringify(nuevoProducto))
     } else {
         alert(`Los datos ingresados no son números. Vuelve a ingresarlos.`);
     }
 }
+
+addProduct.addEventListener("click", () => {
+    agregarProducto();
+});
 
 //CARRITO 1.0
 
@@ -64,11 +75,11 @@ cards.addEventListener("click", e => { addCarrito(e); });
 items.addEventListener("click", e => { btnAccion(e); });
 
 // Get Products
-const fetchData = async () => {
+const fetchData = async (palabra = '', ) => {
     try {
         const rest = await fetch("api.json");
         const data = await rest.json();
-        pintarCard(data);
+        (palabra) ? (filterPrecio(data, palabra))(pintarCard(fillData)) : (pintarCard(data));
     } catch (error) {
         console.log(error);
     }
@@ -76,10 +87,11 @@ const fetchData = async () => {
 
 // Paint Products
 const pintarCard = data => {
+    cards.innerHTML = ''
     data.forEach(producto => {
+        templateCard.querySelector(".card-img-top").setAttribute("src", producto.thumbnaiUrl);
         templateCard.querySelector(".card-title").textContent = producto.nombre;
         templateCard.querySelector(".card-price").textContent = producto.precio;
-        templateCard.querySelector(".card-img-top").setAttribute("src", producto.thumbnaiUrl);
         templateCard.querySelector(".card-btn").dataset.id = producto.id;
         const clone = templateCard.cloneNode(true);
         fragment.appendChild(clone);
@@ -151,7 +163,7 @@ const pintarFooter = () => {
 
     const btnVaciar = document.getElementById("vaciar-carrito");
     btnVaciar.addEventListener("click", () => {
-        
+
         Swal.fire({
             position: 'top-end',
             icon: 'success',
@@ -179,7 +191,7 @@ const btnAccion = e => {
         Toastify({
             text: `Se añadió un ${producto.title} mas al carrito`,
             duration: 1500,
-            style: {background:"#e8e8e8", color:"black"},
+            style: { background: "#e8e8e8", color: "black" },
             close: true
         }).showToast();
     }
@@ -198,21 +210,21 @@ const btnAccion = e => {
         Toastify({
             text: `Se eliminó un ${producto.title} del carrito`,
             duration: 1500,
-            style: {background:"#e8e8e8", color:"black"},
+            style: { background: "#e8e8e8", color: "black" },
         }).showToast();
-    
-    e.stopPropagation();
-}}
 
-// FILTRO PRECIOS
-let menorMayor = document.querySelector(".filterPrecio");
-
-const filterPrecio = () => {
-    let filterUsuario = parseFloat(prompt("¿Hasta que precio quieres buscar?"));
-    let search = fetchData.filter(producto => producto.precio <= filterUsuario);
-    if (search != `undefined`) {
-        pintarCard();
-    } else console.log(`No se ha encontrado productos en ese rango de precio.`);
+        e.stopPropagation();
+    }
 }
 
-menorMayor.addEventListener("click", filterPrecio);
+// FILTRO PRECIOS
+let menorMayor = document.querySelector(".search_box");
+let fillData = {}
+
+const filterPrecio = (data, palabra) => {
+    fillData = Array.from(data.filter(word => word.nombre.toLowerCase().indexOf(palabra) > -1));
+}
+
+menorMayor.addEventListener("change", e => {
+    fetchData(e.target.value)
+});
